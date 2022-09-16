f98117e1a3e4d89872bbc24010ecf03021b38397
Sergio Luiz <sergioj995@hotmail.com>
2022-09-16 15:58:26 -0300

    Resolição da atividade

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string nome, sobrenome;
+System.Console.Write("Digite o seu nome.....:");
+nome = Console.ReadLine()!;
+
+System.Console.Write("Digite seu sobrenome...");
+sobrenome = Console.ReadLine()!;
+
+System.Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Thread.Sleep(1000);
+System.Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()} {nome}");
